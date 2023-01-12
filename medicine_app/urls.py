ca2607252473b6ab40b6e411c938aec25762f084
waregagbagbo <gabelwarega@gmail.com>
2023-01-12 23:15:44 +0300

    added app into the urls and settings section

--- medicine_app/urls.py
+++ medicine_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('meddosage/', include('meddosage.urls')),    
     
 ]
