ede28e2cb778b7ba1e339e79a77acfa31537e2cb
waregagbagbo <gabelwarega@gmail.com>
2023-01-16 17:46:00 +0300

    Created superuser account

--- meddosage/views.py
+++ meddosage/views.py
@@ -12,7 +12,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class  MedicineView(LoginRequiredMixin,ListView):
     model = Medicine
     template_name = 'medicine/medlist.html'
+    context_object_name = 'meds'
     
+    def get_queryset(self, **kwargs):
+        return Medicine.objects.filter(self.request.user).order_by('-tracked_medicine')
+    
+    """We can however use context data 
+    
+    def get_context_data(self, **kwargs):
+    context = super(Medicine, self).get_context_data(self, **kwargs)
+    context[meds] = context.self.request.user.order_by(-tracked_medicine)
+    return context"""
+
+
     
     
     
