bfb104610912d90f5a1ab251bf3360722d175ec6
waregagbagbo <gabelwarega@gmail.com>
2023-01-16 18:00:26 +0300

    rebased all the model migrations

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    country = models.CharField(max_length=10 ,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    country = models.CharField(max_length=10 ,blank=True, null=True)
+    
+    class Meta: 
+        verbose_name = "Profile"
+        
+    def __str__(self):
+        return '%s'% self.username
+    
+    
--- meddosage/models.py
+++ meddosage/models.py
@@ -4,9 +4,9 @@ from accounts.models import UserProfile
 # Create your models here.
 
 class Medicine(models.Model):
-    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    tracked_medicine = models.CharField(max_length=10)
-    dosage = models.CharField(max_length=10)
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
+    tracked_medicine = models.CharField(max_length=10, null=True)
+    dosage = models.CharField(max_length=10, null=True)
     frequency = models.IntegerField()
         
     class Meta:
